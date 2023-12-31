727b27144debd985cabcd42a64e98d4cd6965166
shoyab khan <129213723+mrshoya05@users.noreply.github.com>
2023-12-31 12:30:03 +0000

    data types in javascript

---
+++ .devcontainer/01_basics/01_datatypes.js
@@ -0,0 +1,16 @@
+"use strict" // for new version 
+
+let name = "shoyab"; // string 
+let age = 19; // number 
+let isLoggedIn = true; // boolean
+
+//bigInt for large numbers 
+// string can be used in double or single quotes 
+// null (stand alone value), empty { example temprature values}
+// undefined => when value is not defined 
+// symbol use in react components to find out uniqueness between them 
+
+// objects are non primitive data types...  
+ 
+console.log(typeof null);
+
